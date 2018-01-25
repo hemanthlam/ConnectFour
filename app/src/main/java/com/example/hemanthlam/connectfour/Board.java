3698bb6d3d7090b60a3d0c1638260bc069c12e50
Jacob Crisan <jacob.crisan@gmail.com>
2018-01-25 02:31:34 -0800

    Added Experimental Grid Generator Demo (to show that grid generation works), and modified board class to match initial UML specification.

--- app/src/main/java/com/example/hemanthlam/connectfour/Board.java
+++ app/src/main/java/com/example/hemanthlam/connectfour/Board.java
@@ -3,100 +3,85 @@ package com.example.hemanthlam.connectfour;
 /**
  * Created by Jacob Crisan on 1/22/18.
  * File: Board.java
- * Purpose: to define an object class to represent the connect four game board. This should be able to handle game board operations
+ * Purpose: to define an object class to represent the connect four game board.
  */
 
 public class Board {
     // A multidimensional array to hold the connect four grid. It is a multi-dimensional array of booleans
     // (true indicates disc in appropriate part of grid, with false indicating otherwise)
-    private boolean discSquareArr[][];
-
-    // An array that holds the next available disc space in the grid.
-    // Each index of the array represents a column of the grid, and the integer number at that position represents the index of the next available
-    // For example, if nextUnusedDiscSquareForColumn[2] = 3, that means that for the third column (which is at index 2), the next available disc square is the fourth (not the third since arrays start at zero) from the bottom
-    // 0 starts at the bottom of the board
-    private int nextUnusedDiscForColumn[];
-
-    // A string to specify disc color (could see some change in the future, but for now, its a start)
-    private String discColor;
+    private int boardBounds[][];
 
     // The number of rows and columns in the grid
-    private int numColumns;
-    private int numRows;
+    private int height;
+    private int width;
 
     // Constructors
     public Board() {
-        this.discSquareArr = null;
-        this.nextUnusedDiscForColumn = null;
-        this.numColumns = 0;
-        this.numRows = 0;
-        this.discColor = "black";
+        boardBounds = null;
+        height = 0;
+        width = 0;
     }
     public Board(String size) {
 
-        // Set Disc Board Color
-        this.discColor = "black";
-
         // Allocate disc array (boolean indicates if disc square is active or inactive)
         if (size.equalsIgnoreCase("7x6")) {
-            this.discSquareArr = new boolean[7][6];
-            this.numColumns = 7;
+            this.boardBounds = new int[7][6];
+            this.height = 6;
+            this.width = 7;
         } else if (size.equalsIgnoreCase("8x7")) {
-            this.discSquareArr = new boolean[8][7];
-            this.numColumns = 8;
+            this.boardBounds = new int[8][7];
+            this.width = 8;
+            this.height = 7;
         } else if (size.equalsIgnoreCase("10x8")) {
-            this.discSquareArr = new boolean[10][8];
-            this.numColumns = 10;
+            this.boardBounds = new int[10][8];
+            this.height = 8;
+            this.width = 10;
         }
         // If no valid size is given, we can't allocate anything
         // It might be worth trying to throw an exception in the future, though I don't see this case happening very often
         else {
-            this.discSquareArr = null;
+            this.boardBounds = null;
+            this.height = 0;
+            this.width = 0;
             return;
         }
 
-        // Initialize nextUnusedDiscForColumn array (the number in the array indicates the next index of the column array from which there is an inactive--therefore unused--disc square)
-        this.nextUnusedDiscForColumn = new int[numColumns];
+        // Initialize disc array. Thankfully we don't need to resuse code!
+        this.clearBoard();
+    }
 
-        // Initialize disc array
-        for (int i = 0; i < numColumns; ++i) {
-            this.nextUnusedDiscForColumn[i] = 0;
-            for (int j = 0; j < this.discSquareArr[i].length; ++j) {
-                this.discSquareArr[i][j] = false;
+    // ClearBoard
+    // INPUT: none
+    // OUTPUT: none
+    // Purpose: to clear the internal grid of data so we can start again
+    public void clearBoard() {
+        for (int i = 0; i < this.width; ++i) {
+            for (int j = 0; j < this.height; ++j) {
+                this.boardBounds[i][j] = 0;
             }
         }
     }
 
-    /// Board Functions
-    // Add a disc to a specified column, if possible.
-    // INPUT: column (the column at which you wish to insert a disc)
-    // OUTPUT: true if the disc was added successfully. False if it wasn't (if the column number was too large, for example)
-    /*public boolean AddDiscToColumn(int column) {
+    // findPosition
+    // INPUT: col (int)
+    // OUTPUT: int
+    // Purpose: gets the index of the next availalble row in a specified column.
+    //          When a disc is placed in that row, it no longer becomes available
+    public int findPosition(int col) {
+        // Checking if given column is valid (if the index of the column exists in the array)
+        // I am going to start an arbitrary error int return system, where -2 indicates invalid column input and -1 indicates that there are no available rows in the column
+        if (col < 0 || col > (this.width-1))
+            return -2;
 
-        int nextUnusedDiscSpaceInColumn = 0;
-
-        // Checks for the next available disc space in the desired column using the nextUnusedDiscForColumn array
-        if (column > -1 && column < this.numColumns) {
-            if (nextUnusedDiscForColumn[column] < numRows) {
-                // This looked complicated, but basically, I am getting the next available unused disc in the specified column
-                // And setting it as the second index variable in the discSquareArr call
-                // I will also increment the nextUnusedDiscForColumn
-                discSquareArr[column][nextUnusedDiscForColumn[column]] = true;
-                ++(nextUnusedDiscForColumn[column]);
-
-                // Trigger Animation
-
-                return true;
-            }
-            // If the index of the next unused disc in the column is equal to the number of rows in the board, there is no more space left in the column
-            // Therefore, we can't add to it
-            else {
-                return false;
+        for (int i = 0; i < this.height; ++i) {
+            if (this.boardBounds[col][i] == 0) {
+                // Not sure what to do with the disc block when we discover its open, but this is a start (we can figure out what to do later)
+                boardBounds[col][i] = 1;
+                return i;
             }
         }
-        else {
-            return false;
-        }
+
+        // If nothing was found (which is the case if we get here), return -1 to indicated no available rows were found
+        return -1;
     }
-    */
 }
--- app/src/main/java/com/example/hemanthlam/connectfour/MainActivity.java
+++ app/src/main/java/com/example/hemanthlam/connectfour/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.hemanthlam.connectfour;
 
+import android.content.res.Configuration;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,89 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Where the fun begins
+        generateGrid();
+    }
+
+    public void generateGrid() {
+        // Get main activity window relative layout. We need this when we attach the the Linear Layouts to i
+        RelativeLayout layout = (RelativeLayout)findViewById(R.id.mainActivityRL1);
+
+        // https://stackoverflow.com/questions/4743116/get-screen-width-and-height
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int numColumns = 7;
+        int numRows = 6;
+        Button buttonArr[] = new Button[numRows];
+        int columnWidth;
+        int height;
+
+        // https://stackoverflow.com/questions/3663665/how-can-i-get-the-current-screen-orientation
+        // Change heignt and width depending on screen orientation
+        if (layout.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            columnWidth=1+(displayMetrics.widthPixels / numColumns);
+            height = displayMetrics.widthPixels;
+        }
+        else {
+            columnWidth=1+(displayMetrics.widthPixels / numColumns);
+
+            // HeightPixels doesn't actually get the right window size when the phone is in landscape mode.
+            // I presume it has something to do with screen realistate used by the header of the app
+            // I am not sure how difficult it will be to deal with that later
+            height = displayMetrics.heightPixels - 150;
+        }
+
+        // Create the linear layout of columns
+        for (int i = 0; i < numColumns; ++i) {
+            // Referenced/Looked at: https://stackoverflow.com/questions/7241145/creating-linearlayout-in-java-elements-are-not-shown
+            // Referenced/Looked at: https://stackoverflow.com/questions/10159372/android-view-layout-width-how-to-change-programmatically
+            // Internet also advised to use setMargins
+            // The code block below creates the lienar layout
+            LinearLayout linLayout = new LinearLayout(layout.getContext()/*getApplicationContext()*/);
+            linLayout.setId(i);
+
+            // To assign it parameters, you need to create a layoutParameters object, configure it, and assign it to the object you want to give the parameters to
+            LinearLayout.LayoutParams layoutParamaters = new LinearLayout.LayoutParams(columnWidth, LinearLayout.LayoutParams.WRAP_CONTENT);
+            linLayout.setOrientation(LinearLayout.VERTICAL);
+            linLayout.setLayoutParams(layoutParamaters);
+
+            // Add Linear Layout to main window
+            layout.addView(linLayout);
+
+            // Add some offset so the layouts don't end up stacked on top of each other
+            // It seems that when I didn't do this, android would put the LinearLayouts directly on top of each other
+            linLayout.setX(i*columnWidth);
+
+            // For each liner layout (which represents a column), we now need rows
+            // This loop takes care of that
+            for (int j = 0; j < numRows; j++)
+            {
+                // I am going to use ImageButtons for the grid
+                // Regular buttons had some padding that I didn't like
+                ImageButton imageView = new ImageButton(linLayout.getContext());
+                //imageView.setImageURI(Uri.parse("something!")); <-- The correct URL of an image would go here
+
+                // Referenced/Looked at: https://www.android-examples.com/set-onclicklistener-on-imageview-in-android-example/
+                // I wanted to add OnClick functionality so the image buttons do something when clicked (there is no clicking animation for ImageButtons)
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        v.setBackgroundColor(Color.rgb(120, 250, 90));
+                    }
+                });
+
+                // Referenced/Looked at: https://stackoverflow.com/questions/33336058/how-to-add-buttons-below-each-other-dynamically
+                // More layout paremeter configuration
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, height / numRows);
+                imageView.setLayoutParams(params);
+                imageView.setBackgroundColor(Color.rgb((10*i)+(10*j)+(10*j*i), (20*i)+(20*j)+(20*j*i), (40*i)+(40*j)+(40*j*i)));
+                imageView.setId((i*10)+j);
+
+                // Add the row to the column (add the ImageButton to the LinearLayout)
+                linLayout.addView(imageView);
+            }
+        }
     }
 }
