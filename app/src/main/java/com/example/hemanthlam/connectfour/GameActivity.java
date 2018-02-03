3a161e509f6bbe63a88c43bcf4a7c2dbe4d549d6
Soanmgithub7 <sgyan2@pdx.edu>
2018-02-03 14:26:11 -0800

    Added game winning algorithm and stalemate check algorithm

--- app/src/main/java/com/example/hemanthlam/connectfour/GameActivity.java
+++ app/src/main/java/com/example/hemanthlam/connectfour/GameActivity.java
@@ -1,10 +1,12 @@
 package com.example.hemanthlam.connectfour;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 /**
  * Created by Sean on 1/25/2018.
@@ -17,6 +19,7 @@ public class GameActivity extends AppCompatActivity {
     protected RelativeLayout box;
     protected int turn = 1;
     protected int gameType;
+    private boolean isGameOver;
 
     //Hide all UI pieces from board
     protected void hidePieces(){
@@ -30,15 +33,50 @@ public class GameActivity extends AppCompatActivity {
     }
 
     //Place a disc in the correct slot on the board
+    //if isGameOver dont place discs
     protected void placeDisc(int col){
-        int row = gameBoard.findPosition(col,turn);
-        if(row == -1)
-            return;
-        LinearLayout temp = (LinearLayout) box.getChildAt(col);
-        ImageView chip = (ImageView) temp.getChildAt(row);
-        animate(chip);
-        changeTurn();
+        if(!isGameOver) {
+            int row = gameBoard.findPosition(col, turn);
+            if (row == -1)
+                return;
+            LinearLayout temp = (LinearLayout) box.getChildAt(col);
+            ImageView chip = (ImageView) temp.getChildAt(row);
+            animate(chip);
+            findWinner();
+            changeTurn();
+        }
     }
+
+    //finds if the player who is playing has won
+    public void findWinner(){
+
+        int[][] a = gameBoard.findWinner(turn);
+        final String message = "player" + turn + "won";
+        if(a!=null){
+            isGameOver = true;
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                }
+            }, 100);
+
+        }
+        else {
+            if(gameBoard.checkIfBoardFull()) {
+                isGameOver = true;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Stalemate" , Toast.LENGTH_SHORT).show();
+                    }
+                }, 100);
+
+            }
+        }
+    }
+
     //Switch turns
     protected void changeTurn(){
         if(turn == 1)
